011cb84cf17d94e847ecdcabba7747772ba0fdf7
thirumala95 <23h51a67h6@cmrcet.ac.in>
2025-09-02 23:42:48 +0530

    Time: 4 ms (99.64%), Space: 45.7 MB (25.02%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+              
+                return checkPalindrome(s, left + 1, right) || checkPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
+    private boolean checkPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
