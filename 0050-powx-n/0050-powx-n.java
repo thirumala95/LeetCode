6f0bb29ce06db4befa2d120a2af0019e46fc08e1
thirumala95 <23h51a67h6@cmrcet.ac.in>
2026-06-09 11:37:44 +0530

    Time: 0 ms (100%), Space: 48.3 MB (10.53%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;  // handle overflow case
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1;
+        
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+        
+        return result;
+    }
+}
+
