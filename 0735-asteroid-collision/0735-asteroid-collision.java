8445fde90869f0370210ffb3d8ad94a1c8ec49f4
thirumala95 <23h51a67h6@cmrcet.ac.in>
2026-06-13 15:53:23 +0530

    Time: 4 ms (82.6%), Space: 46.7 MB (81.9%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int a : asteroids) {
+
+            boolean alive = true;
+
+            while (alive &&
+                   a < 0 &&
+                   !stack.isEmpty() &&
+                   stack.peek() > 0) {
+
+                if (stack.peek() < -a) {
+
+                    stack.pop();
+                }
+                else if (stack.peek() == -a) {
+
+                    stack.pop();
+                    alive = false;
+                }
+                else {
+
+                    alive = false;
+                }
+            }
+
+            if (alive) {
+                stack.push(a);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
