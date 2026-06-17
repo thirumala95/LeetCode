6990664b23baba7f24d8082616939229e167f656
thirumala95 <23h51a67h6@cmrcet.ac.in>
2026-06-17 22:36:24 +0530

    Time: 4 ms (76.3%), Space: 44 MB (85.41%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+
+        List<String> result = new ArrayList<>();
+
+        dfs(root, "", result);
+
+        return result;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> result) {
+
+        if (node == null) {
+            return;
+        }
+
+        if (path.length() == 0) {
+            path = String.valueOf(node.val);
+        } else {
+            path += "->" + node.val;
+        }
+
+        // Leaf node
+        if (node.left == null && node.right == null) {
+            result.add(path);
+            return;
+        }
+
+        dfs(node.left, path, result);
+        dfs(node.right, path, result);
+    }
+}
