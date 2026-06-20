53400635584ddd5a8a99daae705853a7057e5201
thirumala95 <23h51a67h6@cmrcet.ac.in>
2026-06-20 22:32:24 +0530

    Time: 5 ms (68.12%), Space: 46 MB (40.23%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'U') y++;
+            else if (ch == 'D') y--;
+            else if (ch == 'R') x++;
+            else x--;
+        }
+
+        return x == 0 && y == 0;
+    }
+}
