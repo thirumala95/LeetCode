b6741f37614266204a4e492aeb4efc22a6268aef
SAPAVAT THIRUMALA <23h51a67h6@cmrcet.ac.in>
2026-08-01 11:06:14 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.79%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        
+        
+        for (int i = 0; i < n; i++) {
+            dp[i] = nums[i];
+        }
+        
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i] = Math.max(nums[i] - dp[i + 1], nums[j] - dp[i]);
+            }
+        }
+        
+       
+        return dp[0] >= 0;
+    }
+}
